56f5060f418c6cc912ccc527851a9012dfe709b1
Max <khryshcheniukmaksym@gmail.com>
2022-03-25 17:43:29 +0200

    ClassForPointIn3D

---
+++ Class_PointIn3D/Class_PointIn3D/Program.cs
@@ -0,0 +1,39 @@
+﻿class PointOn3D
+{
+    public PointOn3D()
+    {
+
+    }
+    public PointOn3D(float x, float y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+    public PointOn3D(float x, float y, float z) : this(x, y)
+    {
+        this.z = z;
+    }
+    public PointOn3D(PointOn3D point)
+    {
+        this.x = point.x;
+        this.y = point.y;
+        this.z = point.z;
+    }
+    private float x, y, z;
+    public void PrintPoint()
+    {
+        Console.WriteLine($"X: {x};\nY: {y};\nZ: {z}.\n");
+    }
+    public void ChangeX(float x)
+    {
+        this.x = x;
+    }
+    public void ChangeY(float y)
+    {
+        this.y = y;
+    }
+    public void ChangeZ(float z)
+    {
+        this.z = z;
+    }
+}
